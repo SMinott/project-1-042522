4c277d40060358355a40079736404a9389d7a9b5
Serje <Serje.havandjian@gmail.com>
2022-05-09 14:48:56 -0700

    We were, thankfully, able to display something on the DOM.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("TEST TEST THIS IS SERJE TESTING")
+document.addEventListener("DOMContentLoaded", ()=>{
+    const url ="https://excuser.herokuapp.com/v1/excuse/200"
+    fetch(url)
+    .then(response =>{
+        return response.json()
+    })
+    .then(response =>{
+        response.forEach(getElementsOnDom)
+    })
+})
+
+
+function getElementsOnDom(response){
+    console.log(response)
+    const quotes = document.getElementById("excuses")
+    const newQuote = document.createElement("li")
+    newQuote.textContent = response.excuse
+    quotes.append(newQuote)
+}
+
