b5da6dc93836e458672fb7046313e558989e64f4
Jeena Park <jeenapark15@gmail.com>
2022-05-10 09:22:57 -0400

    added buttons that show each excuse category and when clicked show their respective excuses

---
+++ index.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(`https://excuser.herokuapp.com/v1/excuse/300`)
+    .then(resp => resp.json())
+    .then(excuses => {
+        addButtonListeners(excuses);
+    });
+});
+
+function addExcusesByCategory(excuses, category) {
+    document.querySelector("ul").textContent = "";
+    let filteredExcuses = excuses.filter((excuse) => {
+        return excuse.category === category;
+    });
+   
+    filteredExcuses.forEach((obj) => {
+        const li = document.createElement("li");
+        li.textContent = obj.excuse;
+        document.querySelector("ul").append(li); 
+    });
+}
+
+function addButtonListeners(excuses) {
+    const familyButton = document.getElementById("family-button");
+    const officeButton = document.getElementById("office-button");
+    const childrenButton = document.getElementById("children-button");
+    const collegeButton = document.getElementById("college-button");
+    const partyButton = document.getElementById("party-button");
+
+    familyButton.addEventListener("click", (e) => {
+        addExcusesByCategory(excuses, "family");
+    });
+
+    officeButton.addEventListener("click", (e) => {
+        addExcusesByCategory(excuses, "office");
+    });
+
+    childrenButton.addEventListener("click", (e) => {
+        addExcusesByCategory(excuses, "children");
+    });
+
+    collegeButton.addEventListener("click", (e) => {
+        addExcusesByCategory(excuses, "college");
+    });
+
+    partyButton.addEventListener("click", (e) => {
+        addExcusesByCategory(excuses, "party");
+    });
+}
